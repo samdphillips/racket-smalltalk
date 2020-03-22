d3ce37af1fbbb9cf45845896f0b18e99034ae16e
Sam Phillips <samdphillips@gmail.com>
2020-03-22 11:36:47 -0700

    Add string literals. Reuse Racket read exns

--- smalltalk-reader/smalltalk/reader/main.rkt
+++ smalltalk-reader/smalltalk/reader/main.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 
 (require racket/match
+         (prefix-in - syntax/readerr)
+         syntax/srcloc
          parser-tools/lex
          (prefix-in : parser-tools/lex-sre))
 
@@ -8,6 +10,25 @@
   (require rackunit
            racket/port
            syntax/parse/define))
+
+(define (->srcloc source pos)
+  (match-define (position offset line column) pos)
+  (vector (if (input-port? source)
+              (object-name source)
+              source)
+          line column offset 0))
+
+(define ((make-raise-read-error	raise-proc) message start-loc end-loc)
+  (let ([srcloc (build-source-location-vector start-loc end-loc)])
+    (raise-proc message
+                (source-location-source   srcloc)
+                (source-location-line     srcloc)
+                (source-location-column   srcloc)
+                (source-location-position srcloc)
+                (source-location-span     srcloc))))
+
+(define raise-read-error (make-raise-read-error -raise-read-error))
+(define raise-read-eof-error (make-raise-read-error -raise-read-eof-error))
 
 (define ZERO (char->integer #\0))
 (define NINE (char->integer #\9))
@@ -50,6 +71,24 @@
 (struct identifier (value) #:transparent)
 (struct keyword (value) #:transparent)
 
+(define (lex-string start-loc input-port)
+  (define out-string (open-output-string))
+  (define lex
+    (lexer
+      [(eof) (raise-read-eof-error "EOF encountered reading string"
+                                   start-loc
+                                   (->srcloc input-port end-pos))]
+      [(:* (:~ #\'))
+       (begin
+         (write-string lexeme out-string)
+         (lex input-port))]
+      [(:: #\' #\')
+       (begin
+         (write-char #\' out-string)
+         (lex input-port))]
+      [#\' (get-output-string out-string)]))
+  (lex input-port))
+
 (define smalltalk-lex
   (lexer-src-pos
     [(eof) (return-without-pos eof)]
@@ -73,6 +112,8 @@
     [(:: (:or #\_ alphabetic)
          (:* alphabetic numeric))
      (identifier (string->symbol lexeme))]
+
+    [(:: #\') (lex-string (->srcloc input-port start-pos) input-port)]
   ))
 
 (module+ test
@@ -84,8 +125,12 @@
             (position-token-token tok))))
       (list pats ...)))
 
-  (test-case "identifier - abc" (check-tokens "abc" (identifier 'abc)))
-  (test-case "keyword - abc:"   (check-tokens "abc:" (keyword 'abc:)))
-  (test-case "comments"         (check-tokens "\"this is a comment\" abc" (identifier 'abc)))
-  (test-case "numbers"          (check-tokens "16rFF raisedTo: 2" 255 (keyword 'raisedTo:) 2))
+  (test-case "identifier - abc"      (check-tokens "abc" (identifier 'abc)))
+  (test-case "keyword - abc:"        (check-tokens "abc:" (keyword 'abc:)))
+  (test-case "comments"              (check-tokens "\"this is a comment\" abc" (identifier 'abc)))
+  (test-case "numbers"               (check-tokens "16rFF raisedTo: 2" 255 (keyword 'raisedTo:) 2))
+  (test-case "strings"               (check-tokens "'one' 'two' 'three'" "one" "two" "three"))
+  (test-case "escaped strings"       (check-tokens "'''one'' two three'" "'one' two three"))
+  (test-case "no string termination"
+    (check-exn exn:fail:read:eof? (lambda () (check-tokens "'oops" ""))))
 )
