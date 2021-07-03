20c873b23b40ce16d07ac893707a95be4f08618c
Sam Phillips <samdphillips@gmail.com>
2021-07-03 11:36:47 -0700

    Reorganize reader

--- smalltalk-reader/info.rkt
+++ smalltalk-reader/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 
-(define collection 'multi)
+(define name "smalltalk-reader")
+(define collection "smalltalk")
 (define version "0.0.1")
 (define deps '("base" "parser-tools-lib"))
 (define build-deps '("base" "rackunit-lib"))
