066e902b59a704824bd07fc7fb54927163f49042
Sam Phillips <samdphillips@gmail.com>
2021-07-05 18:10:31 -0700

    check ci works for parser with git package

---
+++ smalltalk-parser/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name "smalltalk-parser")
+(define collection "smalltalk")
+(define version "0.0.1")
+(define deps
+  '("base" "https://github.com/samdphillips/racket-microparsec.git?path=microparsec-lib"))
+(define build-deps '("base" "rackunit-lib"))
+
