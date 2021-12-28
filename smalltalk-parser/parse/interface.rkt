a6f5eb3d2b36b91db011cb59a5ba373bd9f59c43
Sam Phillips <samdphillips@gmail.com>
2021-12-28 12:21:16 -0800

    [parser] most simple block parsing

--- smalltalk-parser/parse/interface.rkt
+++ smalltalk-parser/parse/interface.rkt
@@ -4,6 +4,7 @@
 
 (provide (all-defined-out))
 
+(define-signature st:block^     (st:block/p))
 (define-signature st:expr^      (st:expr/p))
 (define-signature st:message^   (st:message/p))
 (define-signature st:primary^   (st:identifier/p
