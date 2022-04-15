a5732a1d429c8f2e271bffe8270a1eb3c63b57e0
Sam Phillips <samdphillips@gmail.com>
2022-04-15 09:37:05 -0700

    Simplify test patterns using `match`

--- smalltalk-parser/parse.rkt
+++ smalltalk-parser/parse.rkt
@@ -3,12 +3,12 @@
 (module* test #f
   (require microparsec
            racket/format
+           racket/match
            racket/port
            (only-in racket/stream stream-empty?)
            racket/unit
            rackunit
            smalltalk/reader
-           syntax/parse
            syntax/parse/define
            "parse/block.rkt"
            "parse/expr.rkt"
@@ -39,13 +39,14 @@
           (define-values (parsed rest)
             (parse test/p (port->stream inp)))
           (with-check-info*
-              (list (make-check-info 'parsed parsed))
+              (list (make-check-info 'parsed parsed)
+                    (make-check-info 'pattern 'pat))
             (lambda ()
               (check-true (stream-empty? rest) "unparsed tail")
               (check-true (syntax? parsed) "parsed is not syntax")
               (check-true
-               (syntax-parse parsed
-                 [pat #t]
+               (match (syntax->datum parsed)
+                 ['pat #t]
                  [_ #f])
                "syntax does not match pattern")))))))
 
@@ -56,120 +57,101 @@
           (define-values (parsed rest)
             (parse test/p (port->stream inp)))
           (with-check-info*
-              (list (make-check-info 'parsed parsed))
+              (list (make-check-info 'parsed parsed)
+                    (make-check-info 'patterns '(pats ...)))
             (lambda ()
               (check-true (stream-empty? rest) "unparsed tail")
               (check-true (list? parsed) "parsed is not list of syntax")
               (check-true (andmap syntax? parsed)
                           "parsed is not list of syntax")
               (check-true
-               (syntax-parse #`(#,@parsed)
-                 [(pats ...) #t]
+               (match (map syntax->datum parsed)
+                 [(list 'pats ...) #t]
                  [_ #f])
                "syntax does not match patterns")))))))
 
   (test-parse st:message/p "42" 42)
-  (test-parse st:message/p "a" {~datum a})
+  (test-parse st:message/p "a" a)
   (test-parse st:message/p "a toString"
-              ({~datum #%st:send} {~datum a} {~datum toString}))
+              (#%st:send a toString))
 
   (test-parse st:message/p "123 factorial"
-              ({~datum #%st:send} 123 {~datum factorial}))
+              (#%st:send 123 factorial))
   (test-parse st:message/p "12345 toString size"
-              ({~datum #%st:send}
-               ({~datum #%st:send} 12345 {~datum toString})
-               {~datum size}))
+              (#%st:send (#%st:send 12345 toString) size))
 
   (test-parse st:message/p "3 + 4"
-              ({~datum #%st:send} 3 {~datum +} 4))
+              (#%st:send 3 + 4))
   (test-parse st:message/p "3 + 4 * 2"
-              ({~datum #%st:send}
-               ({~datum #%st:send} 3 {~datum +} 4)
-               {~datum *} 2))
+              (#%st:send (#%st:send 3 + 4) * 2))
   (test-parse st:message/p "3 factorial + 4 factorial"
-              ({~datum #%st:send}
-               ({~datum #%st:send} 3 {~datum factorial})
-               {~datum +}
-               ({~datum #%st:send} 4 {~datum factorial})))
+              (#%st:send (#%st:send 3 factorial) + (#%st:send 4 factorial)))
 
   (test-parse st:message/p "2 raisedTo: 5"
-              ({~datum #%st:send} 2 {~datum raisedTo:} 5))
+              (#%st:send 2 raisedTo: 5))
   (test-parse st:message/p "d at: 0 put: 1"
-              ({~datum #%st:send} {~datum d} {~datum at:put:} 0 1))
+              (#%st:send d at:put: 0 1))
   (test-parse st:message/p "d at: 0 put: x + y"
-              ({~datum #%st:send}
-               {~datum d} {~datum at:put:} 0
-               ({~datum #%st:send} {~datum x} {~datum +} {~datum y})))
+              (#%st:send d at:put: 0 (#%st:send x + y)))
 
   (test-parse st:message/p
               "self new initialize: aCollection; add: 10; + 0; yourself"
-              ({~datum #%st:send*}
-               ({~datum #%st:send} {~datum self} {~datum new})
-               ({~datum initialize:} {~datum aCollection})
-               ({~datum add:} 10)
-               ({~datum +} 0)
-               ({~datum yourself})))
+              (#%st:send* (#%st:send self new)
+                          (initialize: aCollection)
+                          (add: 10)
+                          (+ 0)
+                          (yourself)))
 
   (test-parse st:expr/p
               "Object new"
-              ({~datum #%st:send} {~datum Object} {~datum new}))
+              (#%st:send Object new))
   (test-parse st:expr/p
               "x := 3 "
-              ({~datum #%st:assignment} {~datum x} 3))
+              (#%st:assignment x 3))
   (test-parse st:expr/p
               "x := 3 + 4"
-              ({~datum #%st:assignment}
-               {~datum x}
-               ({~datum #%st:send} 3 {~datum +} 4)))
+              (#%st:assignment x (#%st:send 3 + 4)))
   (test-parse st:expr/p
               "y := x := 3 "
-              ({~datum #%st:assignment}
-               {~datum y}
-               ({~datum #%st:assignment} {~datum x} 3)))
+              (#%st:assignment y (#%st:assignment x 3)))
   (test-parse st:expr/p
               "x := y"
-              ({~datum #%st:assignment}
-               {~datum x} {~datum y}))
+              (#%st:assignment x y))
   (test-parse st:expr/p
               "x := y squared"
-              ({~datum #%st:assignment}
-               {~datum x}
-               ({~datum #%st:send} {~datum y} {~datum squared})))
+              (#%st:assignment x (#%st:send y squared)))
 
   (test-parse st:statement/p
               "x := y"
-              ({~datum #%st:assignment}
-               {~datum x} {~datum y}))
+              (#%st:assignment x y))
   (test-parse st:statement/p
               "^42"
-              ({~datum #%st:return} 42))
+              (#%st:return 42))
   (test-parse st:statement/p
               "^x := y"
-              ({~datum #%st:return}
-               ({~datum #%st:assignment}
-                {~datum x} {~datum y})))
+              (#%st:return (#%st:assignment x y)))
 
   (test-parse* st:statements/p "3" 3)
   (test-parse* st:statements/p "3. 4" 3 4)
 
   (test-parse st:block/p
               "[ 42 ]"
-              ({~datum #%st:block} () () 42))
+              (#%st:block () () 42))
   (test-parse st:block/p
               "[ 42. ]"
-              ({~datum #%st:block} () () 42))
+              (#%st:block () () 42))
   (test-parse st:block/p
               "[ 42. 43 ]"
-              ({~datum #%st:block} () () 42 43))
+              (#%st:block () () 42 43))
   (test-parse st:block/p
               "[|x| x := 42. x + 1 ]"
-              ({~datum #%st:block} () ({~datum x})
-               ({~datum #%st:assignment} {~datum x} 42)
-               ({~datum #%st:send} {~datum x} + 1)))
+              (#%st:block () (x)
+                          (#%st:assignment x 42)
+                          (#%st:send x + 1)))
   (test-parse st:block/p
               "[| | 42 ]"
-              ({~datum #%st:block} () () 42))
+              (#%st:block () () 42))
   (test-parse st:block/p
               "[|| 42 ]"
-              ({~datum #%st:block} () () 42))
+              (#%st:block () () 42))
   )
